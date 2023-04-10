8d9dc103fd544febee871fb8061bef2fc5892c0d
norway-yv <yrjarvederhus@gmail.com>
2023-04-10 12:18:45 +0200

    Minor changes to layout on transport site

--- src/scripts/transport.ts
+++ src/scripts/transport.ts
@@ -3,6 +3,6 @@ function mainLoadTransport(): void {
     $("#buses > h1").text("Bus departures");
     $("#buses > p > a").text("See more departures (ruter.no)");
     $("#trains > h1").text("Train arrivals and departures");
-    $("#trains > p > a").text("See more departures (vy.no)")
+    $("#trains > p > a").text("See more arrivals and departures (vy.no)")
   }
 }
