925687d87cc1226504ee4e93e6b5f14ce1206eb8
norway-yv <yrjarvederhus@gmail.com>
2023-04-13 10:54:45 +0200

    Compilation

--- src/scripts/transport.js
+++ src/scripts/transport.js
@@ -4,5 +4,6 @@ function mainLoadTransport() {
         $("#buses > p > a").text("See more departures (ruter.no)");
         $("#trains > h1").text("Train arrivals and departures");
         $("#trains > p > a").text("See more arrivals and departures (vy.no)");
+        $("title").text("Transport to/from | Gardermoen");
     }
 }
