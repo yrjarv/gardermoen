c167c37a3e7bd8d85f26a40bd4812214930e843a
norway-yv <yrjarvederhus@gmail.com>
2023-04-19 16:22:02 +0200

    Prevent overload of Avinor servers

--- src/scripts/downloadXML.ps1
+++ src/scripts/downloadXML.ps1
@@ -2,5 +2,5 @@ while (1) {
   $WebClient = New-Object System.Net.WebClient
   $WebClient.DownloadFile("https://flydata.avinor.no/XmlFeed.asp?TimeTo=7&airport=OSL&direction=D","./src/scripts/outXML.xml")
   Write-Output "Downloaded"
-  Start-Sleep 10
+  Start-Sleep 360
 }
