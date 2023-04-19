629b3fa5f58501b0cf3d634e649a816b54a025ec
norway-yv <yrjarvederhus@gmail.com>
2023-04-19 20:42:46 +0200

    Change interval to 1 hour

--- src/scripts/downloadXML.ps1
+++ src/scripts/downloadXML.ps1
@@ -1,6 +1,6 @@
 while (1) {
   $WebClient = New-Object System.Net.WebClient
-  $WebClient.DownloadFile("https://flydata.avinor.no/XmlFeed.asp?TimeTo=7&airport=OSL&direction=D","./src/scripts/outXML.xml")
+  $WebClient.DownloadFile("https://flydata.avinor.no/XmlFeed.asp?TimeTo=24&airport=OSL&direction=D","./src/scripts/outXML.xml")
   Write-Output "Downloaded"
-  Start-Sleep 360
+  Start-Sleep 3600
 }
